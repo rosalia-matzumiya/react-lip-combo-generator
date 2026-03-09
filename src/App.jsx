bb481a7514549f1550c09a1358f457b1c39f301c
Rosalía Matzumiya <15949638+rosalia-matzumiya@users.noreply.github.com>
2026-03-08 22:23:06 -0600

    Updated the generator combos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const combos = [
+  { liner: "MAC Oak", lipstick: "MAC Bombshell" },
+  { liner: "MAC Cork", lipstick: "Marc Jacobs Red" },
+  { liner: "MAC Whirl", lipstick: "Charlotte Tilbury Pillow Talk" },
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [combo, setCombo] = useState(null)
+
+  const generateCombo = () => {
+    const random = combos[Math.floor(Math.random() * combos.length)]
+    setCombo(random)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ textAlign: "center", marginTop: "3rem" }}>
+      <h1>Lip Combo Generator</h1>
+
+      <button onClick={generateCombo}>
+        Generate Combo
+      </button>
+
+      {combo && (
+        <div>
+          <p>Lip Liner: {combo.liner}</p>
+          <p>Lipstick: {combo.lipstick}</p>
+        </div>
+      )}
+    </div>
   )
 }
 
