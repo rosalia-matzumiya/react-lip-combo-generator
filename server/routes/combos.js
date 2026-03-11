faa004d94ef3289ba54e82bd81c237b02cf8ce5d
Rosalía Matzumiya <15949638+rosalia-matzumiya@users.noreply.github.com>
2026-03-10 18:03:19 -0600

    Feature: added random combo generator endpoint

--- server/routes/combos.js
+++ server/routes/combos.js
@@ -5,12 +5,30 @@ const router = express.Router();
 const products = require("../data/products.json");
 
 //create category groups
+const liners = products.filter(p => p.type === "Liner");
+const bases = products.filter(p =>
+  ["Bullet", "Crayon"].includes(p.type)
+);
+const toppers = products.filter(p =>
+  ["Gloss", "Balm"].includes(p.type)
+);
 
 //create random combo generator
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 //create endpoint
 router.get("/random", (req, res) => {
-  res.json({ messsage: "Combo generator coming soon" });
+  const liner = randomItem(liners);
+  const base = randomItem(bases);
+  const topper = randomItem(toppers);
+
+  res.json({
+    liner,
+    base,
+    topper
+  });
 });
 
 module.exports = router;
