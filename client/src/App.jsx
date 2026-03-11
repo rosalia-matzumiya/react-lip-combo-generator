bb3888acc5958a3c5a57056c3bb818593c9714c2
Rosalía Matzumiya <15949638+rosalia-matzumiya@users.noreply.github.com>
2026-03-11 14:27:21 -0600

    Feature: added a loading state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from "react"
+import ComboGenerator from "./components/ComboGenerator";
 
 function App() {
-  const [combo, setCombo] = useState(null)
-
-  async function generateCombo() {
-    const response = await fetch("http://localhost:4000/combos/random")
-    const data = await response.json()
-
-    setCombo(data)
-  }
 
   return (
     <div style={{ textAlign: "center", marginTop: "3rem" }}>
       <h1>Lip Combo Generator</h1>
-      <button onClick={generateCombo}>
-        Generate Lip Combo
-      </button>
-      {
-        combo && (
-          <div>
-            <h2>{combo.colorFamily} Combo</h2>
-
-            <p>Liner: {combo.liner.brand} – {combo.liner.shade}</p>
-            <p>Lipstick: {combo.base.brand} – {combo.base.shade}</p>
-            <p>Topper: {combo.topper.brand} – {combo.topper.shade}</p>
-          </div>
-        )
-      }
+      <ComboGenerator />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/ComboGenerator.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+function ComboGenerator() {
+  const [combo, setCombo] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function generateCombo() {
+
+    setLoading(true);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    const response = await fetch("http://localhost:4000/combos/random");
+    const data = await response.json();
+
+    setCombo(data);
+    setLoading(false);
+  }
+
+  return (
+    <div>
+      <button onClick={generateCombo}>
+        Generate Lip Combo
+      </button>
+      {loading && <p>Loading...</p>}
+      {
+        combo && !loading && (
+          <div>
+            <h2>{combo.colorFamily} Combo</h2>
+
+            <p>Liner: {combo.liner.brand} – {combo.liner.shade}</p>
+            <p>Lipstick: {combo.base.brand} – {combo.base.shade}</p>
+            <p>Topper: {combo.topper.brand} – {combo.topper.shade}</p>
+          </div>
+        )
+      }
+    </div>
+  );
+}
+export default ComboGenerator;
