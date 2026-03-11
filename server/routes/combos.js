c892ae31fee78ecabc7983ff904e7ac6f016924e
Rosalía Matzumiya <15949638+rosalia-matzumiya@users.noreply.github.com>
2026-03-10 19:03:05 -0600

    Feature: implemented combo generator with colorFamily filtering

--- server/routes/combos.js
+++ server/routes/combos.js
@@ -17,8 +17,25 @@ const toppers = products.filter(p =>
 
 //create endpoint
 router.get("/random", (req, res) => {
+
+  console.log("Query:", req.query);
+  console.log("ColorFamily:", req.query.colorFamily);
+  const colorFamily = req.query.colorFamily;
+  let basesFiltered = bases;
+  console.log(bases.slice(0, 5));
+  if (colorFamily) {
+    basesFiltered = bases.filter(
+      p =>
+        p.colorFamily.trim().toLowerCase() === colorFamily.trim().toLowerCase()
+    );
+    console.log("Filtered bases:", basesFiltered.length);
+
+    if (basesFiltered.length === 0) {
+      basesFiltered = bases;
+    }
+  }
   const liner = randomItem(liners);
-  const base = randomItem(bases);
+  const base = randomItem(basesFiltered);
   const topper = randomItem(toppers);
 
   res.json({
